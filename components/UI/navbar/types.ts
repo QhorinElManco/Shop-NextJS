6a6e73c716e5add14c53ecb33c2d0179513b878c
Maynor Pineda <sbethuell@gmail.com>
2025-07-19 22:25:44 -0600

    Cambia el tipo de retorno de icon a ReactElement en LinkData

--- components/UI/navbar/types.ts
+++ components/UI/navbar/types.ts
@@ -1,7 +1,8 @@
 import { TablerIconsProps } from '@tabler/icons-react';
+import { ReactElement } from 'react';
 
 export interface LinkData {
-  icon: (props: TablerIconsProps) => Element;
+  icon: (props: TablerIconsProps) => ReactElement;
   label: string;
   href: string;
 }
